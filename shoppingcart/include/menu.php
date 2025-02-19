65d439c2d5a7dfd8c7f9a35701f135f8e80165bc
beemcraf <pomsanbeem9@gmail.com>
2025-02-19 12:19:24 +0000

    update

---
+++ shoppingcart/include/menu.php
@@ -0,0 +1,21 @@
+<head class="py-3 sticky-top bg-light border-bottom shadow-sm">
+    <div class="container">
+        <ul class="nav nav-pills justify-content-center" style="background-color: #f8c8dc; border-radius: 10px; padding: 10px;">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?php echo $base_url; ?>/index.php" style="background-color: #f4a6c0; border-radius: 8px; margin: 0 5px;">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?php echo $base_url; ?>/product-list.php" style="background-color: #f4a6c0; border-radius: 8px; margin: 0 5px;">
+                    Product List
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="<?php echo $base_url; ?>/cart.php" style="background-color: #f4a6c0; border-radius: 8px; margin: 0 5px;">
+                    Cart
+                </a>
+            </li>
+        </ul>
+    </div>
+</head>
